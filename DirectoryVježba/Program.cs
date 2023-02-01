211559cf2af050c79376ff0b35b91b0f48b7e885
Mannequin52 <92926451+Mannequin52@users.noreply.github.com>
2023-02-01 09:22:33 +0100

    Add files via upload

---
+++ DirectoryVježba/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace DirectoryVježba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi putanju direktorija: ");
+            string put = Console.ReadLine();
+
+            //postoji li direktorij
+            if (Directory.Exists(put))
+            {
+                //da- ispisujemo sve datoteke u njemu
+                Console.WriteLine("Datoteke: ");
+                foreach(string datoteka in Directory.GetFiles(put))
+                {
+                    Console.WriteLine(datoteka);
+                }
+            }
+
+            //postoji li poddirektorij TEST
+            string test = put + "\\TEST";
+
+            if (!Directory.Exists(test))
+            {
+                Directory.CreateDirectory(test);
+            }
+
+            //ispis svih poddirektorija
+            Console.WriteLine("Poddirektoriji: ");
+            foreach(string podd in Directory.GetDirectories(test))
+            {
+                Console.WriteLine(podd);
+            }
+
+            //brisanje poddirektorija
+            Directory.Delete(test);
+
+            //ponovno ispisivanje poddirektorija
+            foreach(string podd in Directory.GetDirectories(put))
+            {
+                Console.WriteLine(podd);
+            }
+            Console.ReadKey();
+        }
+    }
+}
